685242143e5198c6588ce905fed13deaf7c9e951
janenyasoro <janenyasoro>
2023-06-20 12:41:45 -0700

    A function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main- prints void
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char c[] = "main";
+	int i;
+
+	for(i = 0; i < 9; i++0
+			{
+			_putchar(C[i]);
+			}
+			_putchar('\n');
+			return (0);
+}
+
