1f9de46dbe21ad818942937101ea397c23f2dde1
janenyasoro <janenyasoro>
2023-06-26 11:57:40 -0700

    A function that returns the length of a string.

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,23 @@
  *  @s - counts the string
  *  Return: string length
  */
-int _strlen(char *s)
-       	int c = 0;
-	for (; *s != '\0'; s++)
-{
-        c++;
+int _strlen(char *s) {
+    int len = 0;
+
+    // Iterate through the string until the null terminator is reached
+    for (; *s != '\0'; s++) {
+        len++;
     }
 
-    return (c);
+    return len;
 }
+
+int main() {
+    char str[] = "Hello, world!";
+    int len = _strlen(str);
+
+    printf("Length of the string: %d\n", len);
+
+    return 0;
+}
+
