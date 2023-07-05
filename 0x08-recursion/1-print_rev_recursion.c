9ed65aa119b428c40c96f620b0376c45f8a49590
janenyasoro <janenyasoro>
2023-07-05 07:14:48 -0700

    A function that prints a string in reverse.

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
  * _print_rev_recursion - calculates the length of a string recursively
- * @s: the string to calculate the length of
- *
+ * @s: the string to calculate the length of the string
  * Return: the length of the string
  */
 void _print_rev_recursion(char *s)
