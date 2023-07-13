3f354ff4756fd1a3c70f69ba2144474e2a2ee314
janenyasoro <janenyasoro>
2023-07-13 11:26:46 -0700

    A function that allocates memory using malloc.

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
-#include <limits.h>
+
 /**
-* malloc_checked - allocate space in the heap for the arg -- store
-* the arg in the space allocated & return pointer to the mem location
-* @b: unsigned int agruement
-* Return: void heap_space
-*/
+ * malloc_checked - allocates memory using malloc, exit(98) if it fails
+ * @b: size of the memory block to be allocated
+ *
+ * Return: pointer to the address of the memory block
+ */
 void *malloc_checked(unsigned int b)
 {
-	void *heap_space;
+	void *block;
 
-	heap_space = malloc(b);
-	if (heap_space == NULL)
+	block = malloc(b);
+	if (block == NULL)
 		exit(98);
-	return (heap_space);
+	return (block);
 }
