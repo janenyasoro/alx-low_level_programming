64926cdeaf20accd4de455018d9c2faab4e24055
janenyasoro <janenyasoro@gmail.com>
2023-07-19 13:37:27 -0700

    A function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * print_name- this functions prints a name
 (* a blank line
@@ -10,7 +11,13 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (name == NULL)
+	{
 		return;
-	(*f)(name);
+	}
+	if (f == NULL)
+	{
+		return;
+	}
+	f(name);
 }
