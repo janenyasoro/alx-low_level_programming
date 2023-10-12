211f4e3abe48def4aab401cc9eb7eaa9c6be46a4
janenyasoro <janenyasoro@gmail.com>
2023-10-12 06:34:11 -0700

    My work on doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,12 +10,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int nodes = 0;
 	
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("%d/n", h->n);
 		nodes++;
 		h = h->next;
 	}
 
-	return(nodes);
+	return (nodes);
 }
